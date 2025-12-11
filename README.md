b2eebc758dd32792c30f274feae52b9d00ca99c2
DEVESH-001 <dd4168@srmist.edu.in>
2025-12-11 16:29:37 +0530

    Setup Coss-UI + Updated Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Prism ~ all in one ai chat app
+
+`Tech Stack`
+
+- NextJS 16
+- TailwindCSS
+- JavaScript
+- Zustand
+- Tanstack-Query
+- PrismaORM
+- PostgreSQL
+- BetterAuth
+- Vercel
+- OpenRouter
+- AI-SDK
+- zod
+- coss-ui
---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Button>Hi Click Me</Button>
+    </div>
+  );
+};
+
+export default Home;
